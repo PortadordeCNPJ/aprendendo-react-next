bfcc424b671b85d09425005fbd649f8491f08634
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2024-05-21 11:18:18 -0300

    Add: useRef sobre Js

--- src/app/useRefTeste/page.tsx
+++ src/app/useRefTeste/page.tsx
@@ -1,15 +1,26 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const UseRefTeste = () => {
     const [name, setName] = useState("");
 
+    const inputRef = useRef();
+
+    const focusInput = () => {
+        inputRef.current.focus();
+    };
+
     return (
         <div>
-            <input value={name} onChange={(e) => setName(e.target.value)} />
+            <input
+                ref={inputRef}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
             <p>Hello! My name is {name}</p>
+            <button onClick={focusInput}>Focus Input</button>
         </div>
     );
 };
 
-export default UseRefTeste
+export default UseRefTeste;
